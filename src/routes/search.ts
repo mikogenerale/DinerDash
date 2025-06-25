890519c9a4a024f849180ceff84d83c68dcf1d12
mkgnrl <mikognrl@gmail.com>
2025-06-26 01:09:01 +0800

    fix: Added an instantation of SearchController

--- src/routes/search.ts
+++ src/routes/search.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 
-import { searchController } from '../controller';
 import searchQueryParamValidator from '../middleware/searchQueryParamValidator';
+import { SearchController } from '../controllers';
 
 const router = Router();
+const searchController = new SearchController()
 
 router.get(
   '/execute',
-  searchQueryParamValidator, 
+  searchQueryParamValidator,
   searchController.search
 );
 
