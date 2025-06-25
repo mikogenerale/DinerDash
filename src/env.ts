cda384eed1accec47c20a8e68281c53aec8ad7b1
mkgnrl <mikognrl@gmail.com>
2025-06-25 19:09:45 +0800

    init

---
+++ src/env.ts
@@ -0,0 +1,23 @@
+import type { ZodError } from 'zod';
+import { z } from 'zod';
+
+const EnvSchema = z.object({
+  NODE_ENV: z.string().default('development'),
+  PORT: z.coerce.number().default(3000),
+});
+
+export type EnvType = z.infer<typeof EnvSchema>;
+
+let env: EnvType;
+
+try {
+  env = EnvSchema.parse(process.env);
+}
+catch (e) {
+  const error = e as ZodError;
+  console.error('Invalid env: ');
+  console.error(error.flatten().fieldErrors);
+  process.exit(1);
+}
+
+export default env;
---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import type { Request, Response } from 'express';
+import express from 'express';
+import env from './env';
+
+
+const app = express()
+
+app.get('/', (_req: Request, res: Response) => {
+  res.json({
+    message: 'Health check',
+  });
+});
+
+
+app.listen(env.PORT, () => {
+  console.log(`App is running at url: http://localhost:${env.PORT}`);
+});
+
