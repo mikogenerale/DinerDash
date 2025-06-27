385dd6f2587d430954edc1100e9aa0a2a4b06087
mkgnrl <mikognrl@gmail.com>
2025-06-27 19:52:57 +0800

    chore: Updated foursquare errors and added ai errors

--- src/errors/foursquare/FSQUnauthorized.ts
+++ src/errors/foursquare/FSQUnauthorized.ts
@@ -1,17 +1,9 @@
 import { StatusCodes } from "http-status-codes";
 import { BaseError } from "../BaseError";
-import { ErrorCodes } from "../ErrorCodes";
 
 class FSQUnauthorized extends BaseError {
-  code: ErrorCodes
-  
-  constructor(
-    message: string, 
-    statusCode?: StatusCodes, 
-    code?: ErrorCodes, 
-  ) {
+  constructor(message: string, statusCode?: StatusCodes) {
     super(statusCode ?? StatusCodes.UNAUTHORIZED, message)
-    this.code = code ?? ErrorCodes.FSQ_RES_UNAUTHORIZED
   }
 }
 
